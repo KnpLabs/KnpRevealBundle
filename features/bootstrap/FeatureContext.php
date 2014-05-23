bf51fa602fe0cfb93791c132b8e39edf5fe8e6d2
tentacode <contact@gabrielpillet.com>
2014-05-23 15:27:59 +0200

    Fixing behat tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,20 +1,12 @@
 <?php
 
-use Behat\Behat\Context\TurnipAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Context\SnippetAcceptingContext;
 
 /**
  * Behat context class.
  */
-class FeatureContext implements TurnipAcceptingContext
+class FeatureContext implements SnippetAcceptingContext
 {
-    /**
-     * Initializes context. Every scenario gets it's own context object.
-     *
-     * @param array $parameters Suite parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-    }
 }
